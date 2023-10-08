991b54c4f7368183b3879b95ebce37cea7663a49
cakuang1 <cakuang1@berkeley.edu>
2023-10-08 02:47:15 -0700

    testing api

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: 'pages/api/:path*',
+        source: '/pages/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5328/api/:path*'
