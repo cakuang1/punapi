8306090ed1af7c011d5b24075e41e81adc74e484
cakuang1 <cakuang1@berkeley.edu>
2023-10-08 03:05:46 -0700

    test

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/pages/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
-      },
-    ]
-  },
+  reactStrictMode: true,
 }
+
 module.exports = nextConfig
