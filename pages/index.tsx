40ea99bf3d584c890d667fd6f96458a58f291a5e
cakuang1 <cakuang1@berkeley.edu>
2023-11-21 14:21:03 -0800

    added a search endpoints

--- pages/index.tsx
+++ pages/index.tsx
@@ -193,6 +193,96 @@ Retrieve a specific pun by providing its unique identifier (ID).
 
 </div>
 
+<div className="rounded">
+
+<p className="font-bold text-3xl mb-2 mt-10"><span className="border-b">GET Pun(s) by Searching</span></p>
+<p className="text-gray-600 mb-4">
+Retrieve a pun(s) by searching
+</p>
+<div className="relative overflow-x-auto">
+<table className="w-full text-sm text-left text-gray-500  ">
+  <thead className="text-xs text-gray-700 uppercase bg-gray-100 ">
+      <tr>
+          <th scope="col" className="px-6 py-3 rounded-l-lg">
+          Request Type
+          </th>
+          <th scope="col" className="px-6 py-3 rounded-r-lg">
+          URL
+          </th>
+      </tr>
+  </thead>
+  <tbody>
+      <tr className="bg-white bg-green-100">
+          <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          <span className="">GET</span> 
+          </th>
+          <td className="px-6 py-4 font-semibold">
+          https://punapi.rest/api/pun/search?{"{query}"}
+          </td>
+      </tr>
+  </tbody>
+</table>
+</div>
+<div>
+        <h2 className="font-semibold text-lg mt-6">Parameters</h2>
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" className="px-6 py-3 rounded-l-lg">
+                        Name
+                    </th>
+                    <th scope="col" className="px-6 py-3 rounded-r-lg">
+                        Type
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Description
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className="bg-white dark:bg-gray-800">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        query
+                    </th>
+                    <td className="px-6 py-4">
+                        string
+                    </td>
+                    <td className="px-6 py-4">
+                        Key words that you are looking for
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+<div>
+<h2 className="font-semibold text-lg mt-2">Request Example</h2>
+<div className="rounded bg-gray-100 border border-gray-300 shadow-md">
+  <pre className="text-sm font-mono p-4 overflow-x-auto">
+      <code>
+        GET <a href="https://punapi.rest/api/pun/2">https://punapi.rest/api/pun/search?query=cookie </a>
+      </code>
+  </pre>
+</div>
+<h2 className="font-semibold text-lg mt-2">Response Example</h2>
+<div className="rounded bg-gray-100 border border-gray-300 shadow-md">
+<div className=" bg-green-100 text-green-400 rounded ">
+  <span className="text-xl font-semibold ml-5"> 200</span>
+</div>
+  <pre className="text-sm font-mono p-4 overflow-x-auto">
+      <code>
+          {`[{`}
+          {"\n"}
+          {"  \"id\": \"4\""}
+            {"\n"}
+          {" \"pun\": \"Why did the cookie cry? Because his father was a wafer so long!\""}
+          {"\n"}
+          {`}]`}
+      </code>
+  </pre>
+</div>
+</div>
+
+</div>
 
 
 <div className="rounded">
